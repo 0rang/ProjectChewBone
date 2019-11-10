9229b0c1cb26446e32dcd8968be1d34582cd961d
Fardin <0rang@users.noreply.github.com>
2019-11-10 05:41:16 +0000

    weird movement demo
    Can't get things to work yet but thought I'd push this

---
+++ ProjectChewBone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform tr;
+
+    public bool teleportSwitch = false;
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.X))
+            teleportSwitch = true;
+        else if (Input.GetKeyUp(KeyCode.X))
+            teleportSwitch = false;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void Hi()
+    {
+        Debug.Log("Hello World");
+    }
+}
